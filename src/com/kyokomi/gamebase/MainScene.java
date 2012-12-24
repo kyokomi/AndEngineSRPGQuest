b1248c9d71b30063724647c31857b6fedc29bbed
kyokomi <kyoko1220adword@gmail.com>
2012-12-24 10:17:42 +0900

    背景画像を表示

--- src/com/kyokomi/gamebase/MainScene.java
+++ src/com/kyokomi/gamebase/MainScene.java
@@ -1,7 +1,13 @@
 package com.kyokomi.gamebase;
 
-import org.andengine.engine.options.EngineOptions;
+import javax.microedition.khronos.opengles.GL10;
+
 import org.andengine.entity.scene.Scene;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.opengl.texture.TextureOptions;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.ui.activity.BaseGameActivity;
 
 /**
@@ -37,7 +43,24 @@ public class MainScene extends Scene {
 	 * イニシャライズ.
 	 */
 	public void init() {
-		
+		// 画像リソースが格納されている場所を指定
+		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
+		// オブジェクトのサイズを指定（480 * 800が収まる2のべき乗）
+		BitmapTextureAtlas bta = new BitmapTextureAtlas(
+				mBaseGameActivity.getTextureManager(), 
+				512, 1024, 
+				TextureOptions.BILINEAR_PREMULTIPLYALPHA); // アルファ値の設定が可能なタイプ
+		// 範囲をメモリ上に読み込み
+		mBaseGameActivity.getTextureManager().loadTexture(bta);
+		// メモリ上に読み込んだ範囲に画像を読み込み。座標(0,0)
+		ITextureRegion btr = BitmapTextureAtlasTextureRegionFactory.createFromAsset(
+				bta, mBaseGameActivity, "main_bg.png", 0, 0);
+		// Spriteをインスタンス化。座標（0,0）
+		Sprite bg = new Sprite(0, 0, btr, mBaseGameActivity.getVertexBufferObjectManager());
+		// Spriteのアルファ値取り扱いを設定
+		bg.setBlendFunction(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
+		// 画面に配置
+		attachChild(bg);
 	}
 	
 	
