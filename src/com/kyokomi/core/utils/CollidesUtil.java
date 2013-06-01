3725935a9acd1b579f8296111c08817cd20e35c1
kyokomi <kyoko1220adword@gmail.com>
2013-06-01 15:05:25 +0900

    マップ移動を実装

--- src/com/kyokomi/core/utils/CollidesUtil.java
+++ src/com/kyokomi/core/utils/CollidesUtil.java
@@ -13,11 +13,22 @@ public class CollidesUtil {
 	 * @return true:フリック / false:フリックとみなさない
 	 */
 	public static boolean isTouchFlick(float[] startPoints, float[] endPoint) {
+		return isTouchFlick(50, startPoints, endPoint);
+	}
+	/**
+	 * タッチ開始位置と終了位置を元にフリックなのか判定.
+	 * float[]は new float[2]で [0]がx座標 [1]がy座標
+	 * @param baseDistance 判定の元となるフリック距離
+	 * @param startPoints タッチ開始位置
+	 * @param endPoint タッチ終了位置
+	 * @return true:フリック / false:フリックとみなさない
+	 */
+	public static boolean isTouchFlick(float baseDistance, float[] startPoints, float[] endPoint) {
 		
 		float xDistance = endPoint[0] -startPoints[0];
 		float yDistance = endPoint[1] -startPoints[1];
 		
-		if (Math.abs(xDistance) < 50 && Math.abs(yDistance) < 50) {
+		if (Math.abs(xDistance) < baseDistance && Math.abs(yDistance) < baseDistance) {
 			return false;
 		}
 		return true;
@@ -40,6 +51,62 @@ public class CollidesUtil {
 		result += 270;
 		
 		return result;
+	}
+	
+	public enum TouchEventFlick {
+		UN_FLICK(0),
+		
+		DOWN_TO_UP(1),
+		UP_TO_DOWN(2),
+		LEFT_TO_RIGHT(3),
+		RIGHT_TO_LEFT(4),
+		;
+		private Integer value;
+		private TouchEventFlick(Integer value) {
+			this.value = value;
+		}
+		public Integer getValue() {
+			return value;
+		}
+		public static TouchEventFlick get(Integer value) {
+			TouchEventFlick[] values = values();
+			for (TouchEventFlick type : values) {
+				if (type.getValue() == value) {
+					return type;
+				}
+			}
+			throw new RuntimeException("find not tag type.");
+		}
+	}
+	
+	/**
+	 * フリック方向チェック.
+	 * @param startPoints
+	 * @param endPoint
+	 * @return
+	 */
+	public static TouchEventFlick checkToushFlick(float[] startPoints, float[] endPoint) {
+		TouchEventFlick touchEventFlick = TouchEventFlick.UN_FLICK;
+		if (CollidesUtil.isTouchFlick(10, startPoints, endPoint)) {
+			// フリックの角度を求める
+			double angle = CollidesUtil.getAngleByTwoPostion(startPoints, endPoint);
+			// 下から上へのフリックを0度に調整
+			angle -= 180;
+			// 下から上へのフリック
+			if (angle > -45 && angle < 45) {
+				touchEventFlick = TouchEventFlick.DOWN_TO_UP;
+			// 上から下へのフリック
+			} else if (angle > 135 && angle < 225) {
+				touchEventFlick = TouchEventFlick.UP_TO_DOWN;
+			// 左から右へのフリック
+			} else if (angle > 45 && angle < 135) {
+				touchEventFlick = TouchEventFlick.LEFT_TO_RIGHT;
+			// 右から左へのフリック
+			} else if ((angle > 225 && angle < 260) || (angle > -90 && angle < -45)) {
+				touchEventFlick = TouchEventFlick.RIGHT_TO_LEFT;
+			}
+		}
+		return touchEventFlick;
 	}
 	
 	/**
--- src/com/kyokomi/srpgquest/scene/SandBoxScene.java
+++ src/com/kyokomi/srpgquest/scene/SandBoxScene.java
@@ -3,10 +3,13 @@ package com.kyokomi.srpgquest.scene;
 import org.andengine.entity.IEntity;
 import org.andengine.entity.modifier.AlphaModifier;
 import org.andengine.entity.modifier.LoopEntityModifier;
+import org.andengine.entity.modifier.MoveModifier;
 import org.andengine.entity.modifier.SequenceEntityModifier;
 import org.andengine.entity.primitive.Line;
+import org.andengine.entity.primitive.Rectangle;
 import org.andengine.entity.scene.IOnSceneTouchListener;
 import org.andengine.entity.scene.Scene;
+import org.andengine.entity.shape.IAreaShape;
 import org.andengine.entity.sprite.AnimatedSprite;
 import org.andengine.entity.sprite.ButtonSprite;
 import org.andengine.entity.sprite.Sprite;
@@ -14,6 +17,8 @@ import org.andengine.input.touch.TouchEvent;
 import org.andengine.util.color.Color;
 
 import com.kyokomi.core.activity.MultiSceneActivity;
+import com.kyokomi.core.utils.CollidesUtil;
+import com.kyokomi.core.utils.CollidesUtil.TouchEventFlick;
 
 import android.graphics.Point;
 import android.graphics.PointF;
@@ -27,6 +32,12 @@ public class SandBoxScene extends SrpgBaseScene
 	private static final int GRID_Y = 64;
 	private static final int SPRITE_SIZE = 64;
 	private static final int BASE_Y = 7;
+	
+	private static final int GRID_SIZE_X = 8;
+	private static final int GRID_SIZE_Y = 8;
+	
+	/** ドラッグ判定用 */
+	private float[] touchStartPoint;
 	
 	/**
 	 * 
@@ -50,18 +61,21 @@ public class SandBoxScene extends SrpgBaseScene
 	 * @param pMapPointF
 	 * @return
 	 */
-	public PointF getMapPointToDispPoint(Point pMapPoint) {
+	public PointF indexToDisp(Point pMapIndex) {
+		return indexToDisp(pMapIndex.x, pMapIndex.y);
+	}
+	public PointF indexToDisp(int x, int y) {
 		PointF dispPointF = new PointF();
 		dispPointF.set(
-				(pMapPoint.y + pMapPoint.x) * (GRID_X / 2), 
-				((BASE_Y - 1) - pMapPoint.x + pMapPoint.y) * (GRID_Y / 2));
+				(y + x) * (GRID_X / 2), 
+				((BASE_Y - 1) - x + y) * (GRID_Y / 2));
 		return dispPointF;
 	}
 	
-	public Point pointToIndex(PointF pDispPointF) {
-		return pointToIndex(pDispPointF.x, pDispPointF.y);
+	public Point dispToIndex(PointF pDispPointF) {
+		return dispToIndex(pDispPointF.x, pDispPointF.y);
 	}
-	public Point pointToIndex(float x, float y) {
+	public Point dispToIndex(float x, float y) {
 		int view_y = (int)((y - GRID_Y * BASE_Y / 2) * 2);
 		return new Point(
 				(int)((x - view_y + GRID_X * 10) / GRID_X - 10),
@@ -78,18 +92,26 @@ public class SandBoxScene extends SrpgBaseScene
 
 	@Override
 	public void init() {
+		touchStartPoint = new float[2];
+		
 		// 背景
 		Sprite bg = getBaseActivity().getResourceUtil().getSprite(
 				"bk/bg_jan.jpg");
 		bg.setPosition(0, 0);
 		bg.setSize(getWindowWidth(), getWindowHeight());
 		attachChild(bg);
+
+		Rectangle mapBaseRect = new Rectangle(0, 0, 
+				getWindowWidth(), getWindowHeight(), 
+				getBaseActivity().getVertexBufferObjectManager());
+		mapBaseRect.setTag(9999999); // TODO:どうにかして
+		mapBaseRect.setColor(Color.TRANSPARENT);
 		
-		showGrid();
+		showGrid(mapBaseRect);
 		
-		for (int x = 0; x < 6; x++) {
-			for (int y = 0; y < 6; y++) {
-				PointF pointF = getMapPointToDispPoint(new Point(x, y));
+		for (int x = 0; x < GRID_SIZE_X; x++) {
+			for (int y = 0; y < GRID_SIZE_Y; y++) {
+				PointF pointF = indexToDisp(new Point(x, y));
 				
 				// グリッド画像
 				Sprite grid = getResourceSprite("grid0.png");
@@ -157,11 +179,13 @@ public class SandBoxScene extends SrpgBaseScene
 				
 				grid.setTag(x * 10 + y + 1000);
 				sprite.setTag(x * 10 + y + 100);
-				attachChild(grid);
-				attachChild(sprite);
+				mapBaseRect.attachChild(grid);
+				mapBaseRect.attachChild(sprite);
 			}
 		}
-		sortChildren();
+		mapBaseRect.sortChildren();
+		
+		attachChild(mapBaseRect);
 		
 		// Sceneのタッチリスナーを登録
 		setOnSceneTouchListener(this);
@@ -201,33 +225,122 @@ public class SandBoxScene extends SrpgBaseScene
 	public void onClick(ButtonSprite pButtonSprite, float pTouchAreaLocalX,
 			float pTouchAreaLocalY) {
 	}
-
+	
+	// TODO: グリッドのはみ出した数から計算できるはず。。。
+	private static final int OVER_START_DISP_X = -220;
+	private static final int OVER_END_DISP_X   = 0;
+	private static final int OVER_START_DISP_Y = 0;
+	private static final int OVER_END_DISP_Y   = 100;
+	private float getStartDispX() {
+		return 0 + OVER_START_DISP_X;
+	}
+	private float getStartDispY() {
+		return 0 + OVER_START_DISP_Y;
+	}
+	private float getEndDispX(IAreaShape entity) {
+		return entity.getWidth() + OVER_END_DISP_X;
+	}
+	private float getEndDispY(IAreaShape entity) {
+		return entity.getHeight() + OVER_END_DISP_Y;
+	}
+	
 	@Override
 	public boolean onSceneTouchEvent(Scene pScene, TouchEvent pSceneTouchEvent) {
 		touchSprite(pSceneTouchEvent.getX(), pSceneTouchEvent.getY());
-		Point mapPoint = pointToIndex(pSceneTouchEvent.getX(), pSceneTouchEvent.getY());
+		
+		// タッチした位置のアニメーションの停止とマスの色を黒にして点滅を停止
+		Rectangle mapBaseRect = getBaseMap();
+		if (mapBaseRect == null) {
+			return false;
+		}
+		// タッチ位置をスクロールを考慮したマップ座標に変換
+		Point mapPoint = dispToIndex(
+				pSceneTouchEvent.getX() - mapBaseRect.getX(), 
+				pSceneTouchEvent.getY() - mapBaseRect.getY());
 		Log.d("", " x = " + mapPoint.x + " y = " + mapPoint.y);
-		int count = getChildCount();
-		for (int i = 0; i < count; i++) {
-			IEntity entity = getChildByIndex(i);
-			if (entity instanceof AnimatedSprite) {
-				if (entity.getTag() == (mapPoint.x * 10 + mapPoint.y + 100)) {
-					((AnimatedSprite) entity).stopAnimation();
-				}
-			} else if (entity instanceof Sprite) {
-				if (entity.getTag() == (mapPoint.x * 10 + mapPoint.y + 1000)) {
-					((Sprite) entity).setColor(Color.BLACK);
-					((Sprite) entity).clearEntityModifiers();
+
+		// スクロールチェック
+		TouchEventFlick touchEventFlick = TouchEventFlick.UN_FLICK;
+		float xDistance = 0;
+		float yDistance = 0;
+		if (pSceneTouchEvent.isActionDown()) {
+			// 開始点を登録
+			touchStartPoint[0] = pSceneTouchEvent.getX();
+			touchStartPoint[1] = pSceneTouchEvent.getY();
+		} else if (pSceneTouchEvent.isActionUp() || pSceneTouchEvent.isActionCancel()) {
+			float[] touchEndPoint = new float[2];
+			touchEndPoint[0] = pSceneTouchEvent.getX();
+			touchEndPoint[1] = pSceneTouchEvent.getY();
+			// フリックチェック
+			touchEventFlick = CollidesUtil.checkToushFlick(touchStartPoint, touchEndPoint);
+			if (touchEventFlick != TouchEventFlick.UN_FLICK) {
+				xDistance = touchEndPoint[0] -touchStartPoint[0];
+				yDistance = touchEndPoint[1] -touchStartPoint[1];
+			}
+		}
+		// スクロール時
+		if (touchEventFlick != TouchEventFlick.UN_FLICK) {
+			// マップをスクロール
+			float moveToX = mapBaseRect.getX() + xDistance;
+			float moveToY = mapBaseRect.getY() + yDistance;
+			// 表示可能領域で補正
+			if (getStartDispX() > moveToX) {
+				moveToX = getStartDispX();
+			}
+			if (getEndDispX(mapBaseRect) < (moveToX + mapBaseRect.getWidth())) {
+				moveToX = getEndDispX(mapBaseRect) - mapBaseRect.getWidth();
+			}
+			if (getStartDispY() > moveToY) {
+				moveToY = getStartDispY();
+			}
+			if (getEndDispY(mapBaseRect) < (moveToY + mapBaseRect.getHeight())) {
+				moveToY = getEndDispY(mapBaseRect) - mapBaseRect.getHeight();
+			}
+			
+			mapBaseRect.registerEntityModifier(new MoveModifier(0.2f, 
+					mapBaseRect.getX(), moveToX,
+					mapBaseRect.getY(), moveToY));
+			
+		// スクロール以外のとき
+		} else {
+			// 押し上げ時
+			if (pSceneTouchEvent.isActionUp()) {
+				int count = mapBaseRect.getChildCount();
+				for (int i = 0; i < count; i++) {
+					IEntity entity = mapBaseRect.getChildByIndex(i);
+					if (entity instanceof AnimatedSprite) {
+						if (entity.getTag() == (mapPoint.x * 10 + mapPoint.y + 100)) {
+							((AnimatedSprite) entity).stopAnimation();
+						}
+					} else if (entity instanceof Sprite) {
+						if (entity.getTag() == (mapPoint.x * 10 + mapPoint.y + 1000)) {
+							((Sprite) entity).setColor(Color.BLACK);
+							((Sprite) entity).clearEntityModifiers();
+						}
+					}
 				}
 			}
 		}
+		
 		return false;
+	}
+	
+	private Rectangle getBaseMap() {
+		Rectangle mapBaseRect = null;
+		int count = getChildCount();
+		for (int i = 0; i < count; i++) {
+			IEntity entity = getChildByIndex(i);
+			if (entity instanceof Rectangle && entity.getTag() == 9999999) {
+				mapBaseRect = (Rectangle) entity;
+			}
+		}
+		return mapBaseRect;
 	}
 	
 	/**
 	 * グリッド表示
 	 */
-	private void showGrid() {
+	private void showGrid(IEntity entity) {
 		
 		// 擬似3D縦線
 //		{
@@ -258,9 +371,9 @@ public class SandBoxScene extends SrpgBaseScene
 //			attachChild(line);
 //		}
 		
-		for (int x = 0; x < 7; x++) {
-			PointF pointStart = getMapPointToDispPoint(new Point(x, 0));
-			PointF pointEnd = getMapPointToDispPoint(new Point(x, 6));
+		for (int x = 0; x <= GRID_SIZE_X; x++) {
+			PointF pointStart = indexToDisp(new Point(x, 0));
+			PointF pointEnd = indexToDisp(new Point(x, GRID_SIZE_X));
 			pointStart.y = pointStart.y + GRID_Y / 2;
 			pointEnd.y = pointEnd.y + GRID_Y / 2;
 			final Line line = new Line(pointStart.x, pointStart.y, pointEnd.x, pointEnd.y, 
@@ -269,11 +382,11 @@ public class SandBoxScene extends SrpgBaseScene
 			line.setColor(Color.WHITE);
 			line.setAlpha(0.5f);
 			line.setZIndex(1);
-			attachChild(line);
+			entity.attachChild(line);
 		}
-		for (int y = 0; y < 7; y++) {
-			PointF pointStart = getMapPointToDispPoint(new Point(0, y));
-			PointF pointEnd = getMapPointToDispPoint(new Point(6, y));
+		for (int y = 0; y <= GRID_SIZE_Y; y++) {
+			PointF pointStart = indexToDisp(new Point(0, y));
+			PointF pointEnd = indexToDisp(new Point(GRID_SIZE_Y, y));
 			pointStart.y = pointStart.y + GRID_Y / 2;
 			pointEnd.y = pointEnd.y + GRID_Y / 2;
 			final Line line = new Line(pointStart.x, pointStart.y, pointEnd.x, pointEnd.y, 
@@ -282,7 +395,7 @@ public class SandBoxScene extends SrpgBaseScene
 			line.setColor(Color.WHITE);
 			line.setAlpha(0.5f);
 			line.setZIndex(1);
-			attachChild(line);
+			entity.attachChild(line);
 		}
 	}
 }
